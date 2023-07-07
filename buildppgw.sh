d656cefa073df021de9f269535e5aeca8635a26a
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-07 11:32:06 +0800

    Update buildppgw.sh

--- buildppgw.sh
+++ buildppgw.sh
@@ -5,4 +5,4 @@ go get -u
 if [ -f /go/ppgw/ppgw ]; then
     rm -f /go/ppgw/ppgw
 fi
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o /go/ppgw/ppgw
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static" -trimpath -o /go/ppgw/ppgw
