cfaa0078629e8ae731338795c5100eb2bb874729
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-09-29 15:30:27 +0800

    ycad sync

--- FILES/etc/config/clash/clash-dashboard/assets/Select-29e929fc.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-dcea5d2f.js";const o="_select_13zm8_1",r={select:o};function m({options:t,selected:c,onChange:l}){return s.jsx("select",{className:r.select,value:c,onChange:l,children:t.map(([e,n])=>s.jsx("option",{value:e,children:n},e))})}export{m as S};
